2411ac0d0b14d6263d26db0d5056211f42de45c7
ghm913 <guohongming97@126.com>
2025-11-07 11:15:34 +0100

    add metrics for availability

---
+++ services/metrics.go
@@ -0,0 +1,63 @@
+package services
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	dto "github.com/prometheus/client_model/go"
+)
+
+var (
+	// total requests
+	RequestsTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: "webpage",
+		Name:      "requests_total",
+		Help:      "Total number of requests made to the monitored webpage",
+	})
+
+	// successful requests
+	RequestsSuccess = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: "webpage",
+		Name:      "requests_success",
+		Help:      "Number of successful requests (matching expected status code)",
+	})
+
+	// current availability percentage
+	AvailabilityPercent = promauto.NewGaugeFunc(
+		prometheus.GaugeOpts{
+			Namespace: "webpage",
+			Name:      "availability_percent",
+			Help:      "Current availability percentage (0-100)",
+		},
+		calculateAvailability,
+	)
+)
+
+// result of a single request
+func RecordRequest(isSuccess bool) {
+	RequestsTotal.Inc()
+	if isSuccess {
+		RequestsSuccess.Inc()
+	}
+}
+
+// calculate current availability
+func calculateAvailability() float64 {
+	total := &dto.Metric{}
+	success := &dto.Metric{}
+
+	err := RequestsTotal.Write(total)
+	if err != nil {
+		return 0
+	}
+
+	err = RequestsSuccess.Write(success)
+	if err != nil {
+		return 0
+	}
+
+	if total.Counter.GetValue() == 0 {
+		return 0
+	}
+
+	return (success.Counter.GetValue() / total.Counter.GetValue()) * 100
+}
